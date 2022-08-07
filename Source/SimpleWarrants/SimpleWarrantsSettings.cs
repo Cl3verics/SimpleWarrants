ed253d8df98736f2f03d95bf44276190946e70d0
James <james@epicguru.co.uk>
2022-08-07 12:23:55 +0200

    Final cleanup, add missing sln.

--- Source/SimpleWarrants/SimpleWarrantsSettings.cs
+++ Source/SimpleWarrants/SimpleWarrantsSettings.cs
@@ -58,27 +58,11 @@ namespace SimpleWarrants
         {
             base.ExposeData();
             SimpleSettings.AutoExpose(this);
-            //Scribe_Values.Look(ref chanceOfWarrantsMadeOnColonist, nameof(chanceOfWarrantsMadeOnColonist), 0.05f);
-            //Scribe_Values.Look(ref enableWarrantsOnColonists, nameof(enableWarrantsOnColonists), true);
-            //Scribe_Values.Look(ref enableWarrantsOnArtifact, nameof(enableWarrantsOnArtifact), true);
-            //Scribe_Values.Look(ref enableWarrantsOnAnimals, nameof(enableWarrantsOnAnimals), true);
-            //Scribe_Values.Look(ref enableWarrantRewardScaling, nameof(enableWarrantRewardScaling), true);
         }
 
         public void DoSettingsWindowContents(Rect inRect)
         {
             SimpleSettings.DrawWindow(this, inRect);
-
-            //Rect rect = new Rect(inRect.x, inRect.y, inRect.width, inRect.height);
-            //Listing_Standard listingStandard = new Listing_Standard();
-            //listingStandard.Begin(rect);
-            //listingStandard.Label("SW.ChanceOfWarrantsMadeOnColonist".Translate((chanceOfWarrantsMadeOnColonist * 100f).ToStringDecimalIfSmall() + "%"));
-            //chanceOfWarrantsMadeOnColonist = listingStandard.Slider(chanceOfWarrantsMadeOnColonist, 0, 1f);
-            //listingStandard.CheckboxLabeled("SW.EnableWarrantsOnColonists".Translate(), ref enableWarrantsOnColonists);
-            //listingStandard.CheckboxLabeled("SW.EnableWarrantsOnArtifact".Translate(), ref enableWarrantsOnArtifact);
-            //listingStandard.CheckboxLabeled("SW.EnableWarrantsOnAnimals".Translate(), ref enableWarrantsOnAnimals);
-            //listingStandard.CheckboxLabeled("SW.EnableWarrantRewardScaling".Translate(), ref enableWarrantRewardScaling);
-            //listingStandard.End();
         }
     }
 }
